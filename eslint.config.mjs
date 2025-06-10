a3a099067a7fa35086f5987050bb0fcd3e98e71c
RishabhSharma17 <rishabhsharma17aug2004@gmail.com>
2025-06-10 16:50:01 +0530

    update a lint file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const _dirname = dirname(_filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // Add your own rules to ignore specific errors
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "import/no-anonymous-default-export": "off",
+      "react/display-name": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
